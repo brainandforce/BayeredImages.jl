855eab06ca406cc48784a67b49e7f4c5d4d398a2
Brandon Flores <bsflores@wisc.edu>
2026-04-09 18:06:19 -0500

    Added `permutedims` and cropping

--- src/images.jl
+++ src/images.jl
@@ -14,3 +14,24 @@ Base.size(ci::CFAImage) = size(ci.image)
 Base.IndexStyle(::Type{CFAImage{<:ColorFilterArray,<:Any,M}}) where M = IndexStyle(M)
 @propagate_inbounds Base.getindex(ci::CFAImage, i...) = getindex(ci.image, i...)
 @propagate_inbounds Base.setindex!(ci::CFAImage, x, i...) = setindex!(ci.image, x, i...)
+
+# Shift the CFA over when cropping an image:
+# we know to do this if we're indexing by AbstractUnitRange.
+# Otherwise, just return an AbstractMatrix
+@propagate_inbounds function Base.getindex(ci::CFAImage, i::AbstractUnitRange, j::AbstractUnitRange)
+    return CFAImage(circshift(ci.cfa, first.((i, j)) .- (true, true)), ci.image[i, j])
+end
+
+#---Transforming images while preserving the CFA arrangement---------------------------------------#
+
+#= TODO: ensure that the offsets are handled correctly
+Base.reverse!(ci::CFAImage, dims) = CFAImage(reverse(ci.cfa, dims), reverse!(ci.image, dims))
+Base.reverse(ci::CFAImage, dims) = CFAImage(reverse(ci.cfa, dims), reverse(ci.image, dims))
+=#
+
+#= TODO: handle cases where the image dimensions do not divide the CFA dimensions
+function Base.circshift(ci::CFAImage, shifts)
+end
+=#
+
+Base.permutedims(ci::CFAImage) = CFAImage(permutedims(ci.cfa), permutedims(ci.image))
