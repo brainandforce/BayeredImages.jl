9a3c4c64d96c425bd98f9874b796819e150f5299
Brandon Flores <bsflores@wisc.edu>
2026-04-09 22:39:56 -0500

    Added function for getting Bayered images from FITS files

--- ext/BayeredImagesFITSIOExt.jl
+++ ext/BayeredImagesFITSIOExt.jl
@@ -3,7 +3,7 @@ module BayeredImagesFITSIOExt
 using BayeredImages
 using FITSIO
 
-import BayeredImages: BayerCFA
+import BayeredImages: BayerCFA, BayeredImage
 
 """
     BayerCFA(hdu::HDU)
@@ -13,7 +13,31 @@ Generates the Bayer matrix associated with a FITS file.
 Raw images taken with a camera using a Bayer color filter array will generally include the
 keyword `"BAYERPAT"`, which encodes the arrangement of the Bayer matrix in a format string.
 """
-BayerCFA(hdu::HDU) = BayerCFA(first(read_key(hdu, "BAYERPAT")))
+function BayerCFA(hdu::HDU)
+    bayerpat = try
+        read_key(hdu, "BAYERPAT")
+    catch
+        throw(KeyError("BAYERPAT"))
+    end
+    return BayerCFA(first(bayerpat))
+end
+
 BayerCFA(fits::FITS, index = 1) = BayerCFA(fits[index])
+
+function BayeredImage(hdu::ImageHDU)
+    cfa = try
+        BayerCFA(hdu)
+    catch e
+        e isa KeyError && @error "The BAYERPAT keyword is missing. Is this a CFA image?"
+        rethrow()
+    end
+    return BayeredImage(cfa, read(hdu))
+end
+
+BayeredImage(fits::FITS, hduindex = 1) = BayeredImage(fits[hduindex])
+
+# TODO: get a section of the image with array indices
+
+BayeredImage(::HDU) = error("this HDU does not contain an image")
 
 end
