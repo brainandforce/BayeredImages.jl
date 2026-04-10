9201429f4d5f18944f6e59f253df0bb088602e89
Brandon Flores <bsflores@wisc.edu>
2026-04-09 21:50:31 -0500

    Added docstring and constructor for `BayeredImage`

--- src/images.jl
+++ src/images.jl
@@ -8,7 +8,15 @@ struct CFAImage{C<:ColorFilterArray,T,M<:AbstractMatrix{T}} <: AbstractMatrix{T}
     image::M
 end
 
-const BayeredImage{T,M} = CFAImage{BayerCFA,T,M}
+"""
+    BayeredImage{T,M} <: AbstractMatrix{T}
+
+A raw image taken by a sensor with a Bayer matrix.
+Alias for [`CFAImage{BayerCFA,T,M}`](@ref).
+"""
+const BayeredImage{T,M<:AbstractMatrix{T}} = CFAImage{BayerCFA,T,M}
+
+CFAImage{C}(cfa, image::M) where {C,T,M<:AbstractMatrix{T}} = CFAImage{C,T,M}(cfa, image)
 
 Base.size(ci::CFAImage) = size(ci.image)
 Base.IndexStyle(::Type{CFAImage{<:ColorFilterArray,<:Any,M}}) where M = IndexStyle(M)
