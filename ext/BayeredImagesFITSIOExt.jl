e705637d44f949328856dc5fabf383dc22204cc5
Brandon Flores <bsflores@wisc.edu>
2026-04-12 20:53:53 -0500

    Implemented `ROWORDER` keyword support

--- ext/BayeredImagesFITSIOExt.jl
+++ ext/BayeredImagesFITSIOExt.jl
@@ -24,17 +24,36 @@ end
 
 BayerCFA(fits::FITS, hduindex = 1) = BayerCFA(fits[hduindex])
 
-function BayeredImage(hdu::ImageHDU)
+"""
+    BayeredImage(hdu::ImageHDU; permute = true)
+    BayeredImage(fits::FITS, index=1; permute = true)
+
+Constructs a `BayeredImage` from the specified header data unit of a FITS file.
+
+If `permute = true`, the rows and columns of the output are permuted to comply with Julia
+column-first indexing.
+"""
+function BayeredImage(hdu::ImageHDU; permute=true)
     cfa = try
-        BayerCFA(hdu)
+        permute ? permutedims(BayerCFA(hdu)) : BayerCFA(hdu)
     catch e
         e isa KeyError && @error "The BAYERPAT keyword is missing. Is this a CFA image?"
         rethrow()
     end
-    return BayeredImage(cfa, read(hdu))
+    # Read the image with the correct row order
+    data = permute ? permutedims(read(hdu)) : read(hdu)
+    roworder = try
+        uppercase(first(read_key(hdu, "ROWORDER")))
+    catch
+        "BOTTOM-UP"
+    end
+    # Bottom-up is the default
+    # Only reverse if TOP-DOWN is explicitly stated
+    roworder == "TOP-DOWN" && reverse!(data, dims=1)
+    return BayeredImage(cfa, data)
 end
 
-BayeredImage(fits::FITS, hduindex = 1) = BayeredImage(fits[hduindex])
+BayeredImage(fits::FITS, hduindex = 1; permute = true) = BayeredImage(fits[hduindex]; permute)
 
 # TODO: get a section of the image with array indices
 
